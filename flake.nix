ce7c85607e360fc516d3c17c3a73777a0770cd7b
haennes <hannes.hofmuth@gmail.com>
2024-04-29 22:54:04 +0200

    unneccesary

--- flake.nix
+++ flake.nix
@@ -3,15 +3,7 @@
     "A simple to use wireguard wrapper to connect multiple networks";
 
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
-  outputs = { self, nixpkgs, ... }:
-    let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-    in {
+  outputs = {...}:{
       nixosModules = rec {
         wireguard-wrapper = import ./wireguard-wrapper.nix;
         default = wireguard-wrapper;
